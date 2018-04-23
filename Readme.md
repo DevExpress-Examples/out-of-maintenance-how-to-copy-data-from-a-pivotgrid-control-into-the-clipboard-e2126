1065eb582a2240107b8d0cb874c4be5152f27168
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,13 @@
+# How to: Copy Data from a PivotGrid Control into the Clipboard
+
+
+<p>The following example demonstrates how to copy data from the <strong>PivotGridControl</strong> into the system clipboard.</p>
+
+
+<h3>Description</h3>
+
+<p>First, it is necessary to add the <strong>ContextMenu</strong> with a single &quot;Copy to Clipboard&quot; menu item to a <strong>PivotGridControl</strong>&#39;s <strong>ContextMenu</strong> property. This context menu is invoked when an end-user right-clicks within the Data Area. Then this menu&#39;s <strong>Click</strong> event can be handled to copy data to the clipboard. To copy the selected cells to the clipboard, the <strong>PivotGridControl.CopySelectionToClipboard</strong> method is used.</p><p>To determine whether the selection is not empty, and so whether it is necessary to invoke the context menu, the <strong>PivotGridControl.ContextMenuOpening</strong> event should be handled.</p>
+
+<br/>
+
+
---
+++ VB/HowToCopyClipboard/MainWindow.xaml.vb
@@ -0,0 +1,35 @@
+﻿Imports Microsoft.VisualBasic
+Imports System.Data
+Imports System.Data.OleDb
+Imports System.Windows
+Imports System.Windows.Controls
+Imports DevExpress.Xpf.PivotGrid
+Imports HowToBindToMDB.NwindDataSetTableAdapters
+
+Namespace HowToBindToMDB
+	Partial Public Class MainWindow
+		Inherits Window
+		Private salesPersonDataTable As New NwindDataSet.SalesPersonDataTable()
+		Private salesPersonDataAdapter As New SalesPersonTableAdapter()
+
+		Public Sub New()
+			InitializeComponent()
+			pivotGridControl1.DataSource = salesPersonDataTable
+		End Sub
+
+		Private Sub Window_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
+			salesPersonDataAdapter.Fill(salesPersonDataTable)
+		End Sub
+
+		Private Sub CopyToClipboard_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
+			pivotGridControl1.CopySelectionToClipboard()
+		End Sub
+
+        Private Sub pivotGridControl1_ContextMenuOpening(ByVal sender As Object, _
+                                                         ByVal e As ContextMenuEventArgs)
+            If pivotGridControl1.Selection.IsEmpty Then
+                e.Handled = True
+            End If
+        End Sub
+	End Class
+End Namespace
