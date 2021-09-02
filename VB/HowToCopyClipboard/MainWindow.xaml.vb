4678ea4711876fa23844d9c1dc0f8da306197024
DevExpressExampleBot <fakeEmail>
2021-09-02 18:13:05 +0300

    autogenerate VB

--- VB/HowToCopyClipboard/MainWindow.xaml.vb
+++ VB/HowToCopyClipboard/MainWindow.xaml.vb
@@ -1,35 +1,28 @@
-﻿Imports Microsoft.VisualBasic
-Imports System.Data
-Imports System.Data.OleDb
-Imports System.Windows
+﻿Imports System.Windows
 Imports System.Windows.Controls
-Imports DevExpress.Xpf.PivotGrid
 Imports HowToBindToMDB.NwindDataSetTableAdapters
 
 Namespace HowToBindToMDB
 	Partial Public Class MainWindow
 		Inherits Window
-		Private salesPersonDataTable As New NwindDataSet.SalesPersonDataTable()
-		Private salesPersonDataAdapter As New SalesPersonTableAdapter()
 
+'INSTANT VB NOTE: The variable salesPersonDataTable was renamed since it may cause conflicts with calls to static members of the user-defined type with this name:
+		Private salesPersonDataTable_Conflict As New NwindDataSet.SalesPersonDataTable()
+		Private salesPersonDataAdapter As New SalesPersonTableAdapter()
 		Public Sub New()
 			InitializeComponent()
-			pivotGridControl1.DataSource = salesPersonDataTable
+			pivotGridControl1.DataSource = salesPersonDataTable_Conflict
 		End Sub
-
 		Private Sub Window_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
-			salesPersonDataAdapter.Fill(salesPersonDataTable)
+			salesPersonDataAdapter.Fill(salesPersonDataTable_Conflict)
 		End Sub
-
 		Private Sub CopyToClipboard_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
 			pivotGridControl1.CopySelectionToClipboard()
 		End Sub
-
-        Private Sub pivotGridControl1_ContextMenuOpening(ByVal sender As Object, _
-                                                         ByVal e As ContextMenuEventArgs)
-            If pivotGridControl1.Selection.IsEmpty Then
-                e.Handled = True
-            End If
-        End Sub
+		Private Sub pivotGridControl1_ContextMenuOpening(ByVal sender As Object, ByVal e As ContextMenuEventArgs)
+			If pivotGridControl1.Selection.IsEmpty Then
+				e.Handled = True
+			End If
+		End Sub
 	End Class
 End Namespace
