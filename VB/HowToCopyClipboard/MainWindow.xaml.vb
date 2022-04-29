31fa6213dd54cb17629899c5d3bd18d8ca329028
DevExpressExampleBot <fakeEmail>
2022-04-29 17:18:45 +0400

    Source auto update [skip ci]

--- VB/HowToCopyClipboard/MainWindow.xaml.vb
+++ VB/HowToCopyClipboard/MainWindow.xaml.vb
@@ -1,35 +1,31 @@
-﻿Imports Microsoft.VisualBasic
-Imports System.Data
-Imports System.Data.OleDb
 Imports System.Windows
 Imports System.Windows.Controls
-Imports DevExpress.Xpf.PivotGrid
 Imports HowToBindToMDB.NwindDataSetTableAdapters
 
 Namespace HowToBindToMDB
-	Partial Public Class MainWindow
-		Inherits Window
-		Private salesPersonDataTable As New NwindDataSet.SalesPersonDataTable()
-		Private salesPersonDataAdapter As New SalesPersonTableAdapter()
 
-		Public Sub New()
-			InitializeComponent()
-			pivotGridControl1.DataSource = salesPersonDataTable
-		End Sub
+    Public Partial Class MainWindow
+        Inherits Window
 
-		Private Sub Window_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
-			salesPersonDataAdapter.Fill(salesPersonDataTable)
-		End Sub
+        Private salesPersonDataTable As NwindDataSet.SalesPersonDataTable = New NwindDataSet.SalesPersonDataTable()
 
-		Private Sub CopyToClipboard_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
-			pivotGridControl1.CopySelectionToClipboard()
-		End Sub
+        Private salesPersonDataAdapter As SalesPersonTableAdapter = New SalesPersonTableAdapter()
 
-        Private Sub pivotGridControl1_ContextMenuOpening(ByVal sender As Object, _
-                                                         ByVal e As ContextMenuEventArgs)
-            If pivotGridControl1.Selection.IsEmpty Then
-                e.Handled = True
-            End If
+        Public Sub New()
+            Me.InitializeComponent()
+            Me.pivotGridControl1.DataSource = salesPersonDataTable
         End Sub
-	End Class
+
+        Private Sub Window_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
+            salesPersonDataAdapter.Fill(salesPersonDataTable)
+        End Sub
+
+        Private Sub CopyToClipboard_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
+            Me.pivotGridControl1.CopySelectionToClipboard()
+        End Sub
+
+        Private Sub pivotGridControl1_ContextMenuOpening(ByVal sender As Object, ByVal e As ContextMenuEventArgs)
+            If Me.pivotGridControl1.Selection.IsEmpty Then e.Handled = True
+        End Sub
+    End Class
 End Namespace
